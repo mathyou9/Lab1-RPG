8c4d866a87a3ee5692d5723658c19cb991feb483
mathyou <41449510+mathyou@users.noreply.github.com>
2018-09-19 11:17:11 -0600

    Added necessary files

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Arena.h"
 
 
+
 const int NUM_FILES = 5; // the total number of files to be read from
 
 const std::string fileArray[NUM_FILES] = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt" }; // the string aray containing the file names
